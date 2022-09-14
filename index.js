a128bbf53c52341f35c59045408cb0ffd1c549e6
Tea Aleksov <aleksov.tea@gmail.com>
2022-09-14 22:36:15 +0200

    update/still not fully working

--- index.js
+++ index.js
@@ -9,11 +9,14 @@ const typesOfTask = document.querySelector(".types")
 const resetBtn = document.querySelector(".reset")
 
 
-window.addEventListener("load", getItemsFromLocalStorage());
+window.addEventListener("load", onLoad());
 
-resetBtn.addEventListener('click',reset)
-function reset(){
-    localStorage.clear()
+function onLoad(){
+    if(localStorage){
+        getItemsFromLocalStorage()
+    }else{
+        listContainer += `<h1>Nothing here</h1>`
+    }
 }
 addTaskBtn.addEventListener('click', displayNewTaskSettings)
 saveNewTaskBtn.addEventListener('click',saveNewTask)
@@ -50,31 +53,39 @@ function completedTask(){
                 this.parentNode.style.textDecoration = "line-through";
                 this.parentNode.style.opacity = "0.5";
                 this.remove()
-                console.log("awfcaf")
             }
-            console.log("alLl")
         }
-        console.log("whwhw")
 }
 function deleteTask(){
     let current_tasks = document.querySelectorAll(".delete-item");
         for(let i=0; i<current_tasks.length; i++){
+            
             current_tasks[i].onclick = function(){
+                let list = localStorage.getItem("todoList")
+                let item = current_tasks[i].parentNode.querySelector(".todo-item").textContent
+                console.log(list)  
+                localStorage.removeItem(item)
+                console.log(list)
                 this.parentNode.remove();
-                console.log("awfcaf")
+                
+                
+                // localStorage.removeItem(todoList[i])
             }
-            console.log("alLl")
         }
-}
-
+    }
 function saveNewTask(){
     const task = taskInput.value
     const type = selectType()
     todoList.push(task)
+
+    localStorage.setItem("todoList", todoList)
+    
     const newTask = createNewTask(task, type)
     localStorage.setItem(task,type)
+    
     taskInput.value = ""
     makeTaskBox.style.display = "none"
+    console.log(todoList)
 }
 
 function createNewTask(task, type){
@@ -83,17 +94,21 @@ function createNewTask(task, type){
     <button class="complete-item" onclick="completedTask()">Yes</button>
     <button class="delete-item" onclick="deleteTask()">X</button>    
     </li>`
-    listContainer.innerHTML += template
+    listContainer.innerHTML = template + listContainer.innerHTML
     
 }
-
 function getItemsFromLocalStorage(task){
     for(var i=0;  i<localStorage.length; i++){
+        var list = localStorage.getItem("todoList")
         var key = localStorage.key(i);
-        console.log(localStorage)
+        console.log(key)        
         var value = localStorage[key];
-        // console.log(key + " => " + value);
-        const newTask = createNewTask(key, value)
+
+        for(var i=0; i<list.length; i+=2 ){
+            const newTask = createNewTask(list[i], value)
+        }
+        
+        // console.log(list)
     }
 }
 
